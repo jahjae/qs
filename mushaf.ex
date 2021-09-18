a653ed778537238b5099295559565c79591c1bfb
Jr. Bonn <jahjae@gmail.com>
2021-09-19 00:40:41 +0700

    jahjae

--- mushaf.ex
+++ mushaf.ex
@@ -1,4 +1,6 @@
 defmodule Mushaf do
+
+
   def read() do
     receive do
       {cid, :page, x} -> send(cid, {:page, x})
@@ -6,4 +8,19 @@ defmodule Mushaf do
     end
 
   end
+
+  def load() do
+    kode = File.read!("data/db/mushaf.db") |> String.split("\r\n")
+    unicode(kode)
+  end
+
+  def unicode([]) do
+    :ok
+  end
+  def unicode(x) do
+    [head | tail] = x
+    IO.puts(head)
+    unicode(tail)
+  end
+
 end
