b4aaeaf9cc0a36384dbd642599181c99754dd6bb
Jr. Bonn <jahjae@gmail.com>
2021-09-19 00:59:14 +0700

    jahjae

--- mushaf.ex
+++ mushaf.ex
@@ -1,15 +1,17 @@
 defmodule Mushaf do
   def read() do
     receive do
-      {cid, :page, x} -> send(cid, {:page, x})
-      {cid, :sura, x} -> send(cid, {:sura, x})
+      {cid, :halaman, x} -> send(cid, {:halaman, x})
+      {cid, :juz, x} -> send(cid, {:juz, x})
+      {cid, :surat, x} -> send(cid, {:surat, x})
+      {cid, :ayat, x} -> send(cid, {:ayat, x})
     end
 
   end
 
   def load() do
-    kode = File.read!("data/db/mushaf.db") |> String.split("\r\n")
-    unicode(kode)
+    huruf = File.read!("data/db/mushaf.db") |> String.split("\r\n")
+    unicode(huruf)
   end
 
   def unicode([]) do
@@ -17,9 +19,9 @@ defmodule Mushaf do
   end
 
   def unicode(x) do
-    [head | tail] = x
-    IO.puts(head)
-    unicode(tail)
+    [first | next] = x
+    IO.puts(first)
+    unicode(next)
   end
 
 end
--- qs.ex
+++ qs.ex
@@ -1,20 +1,21 @@
 defmodule Quran do
   def page(x) do
     pid = spawn(Mushaf, :read, [])
-    spawn(Quran, :read, [pid, :page, x])
+    spawn(Quran, :read, [pid, :halaman, x])
 
   end
   def sura(x) do
     sid = spawn(Mushaf, :read, [])
-    spawn(Quran, :read, [sid,:sura, x])
+    spawn(Quran, :read, [sid,:surat, x])
 
   end
   def read(i, s, x) do
     send(i, {self(), s, x})
     receive do
-      {:page, x} -> IO.puts(x)
-      {:sura, x} -> IO.puts(x)
-
+      {:halaman, x} -> IO.puts(x)
+      {:juz, x} -> IO.puts(x)
+      {:surat, x} -> IO.puts(x)
+      {:ayat, x} -> IO.puts(x)
     end
   end
 end
