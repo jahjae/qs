0438b9cdf8b9ee871ebf1043a365c8cfddedd27e
Jr. Bonn <jahjae@gmail.com>
2021-09-18 21:54:18 +0700

    jahjae

--- mushaf.ex
+++ mushaf.ex
@@ -1,5 +1,5 @@
 defmodule Mushaf do
-  def read do
+  def read() do
     receive do
       {cid, :page, x} -> send(cid, {:page, x})
       {cid, :sura, x} -> send(cid, {:sura, x})
--- qs.ex
+++ qs.ex
@@ -1,19 +1,19 @@
 defmodule Quran do
   def page(x) do
-    pid = spawn(Mushaf, read, {})
-    spawn(Quran, read, {pid, :page, x})
+    pid = spawn(Mushaf, :read, [])
+    spawn(Quran, :read, [pid, :page, x])
 
   end
   def sura(x) do
-    sid = spawn(Mushaf, read, {})
-    spawn(Quran, read, {sid,:sura, x})
+    sid = spawn(Mushaf, :read, [])
+    spawn(Quran, :read, [sid,:sura, x])
 
   end
-  def read({i, s, x}) do
+  def read(i, s, x) do
     send(i, {self(), s, x})
     receive do
-      {:page, x} -> x
-      {:sura, x} -> x
+      {:page, x} -> IO.puts(x)
+      {:sura, x} -> IO.puts(x)
 
     end
   end
