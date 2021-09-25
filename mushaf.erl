2d659099a25aa5a5bc9c79e269eb0254dd3c1d58
Jr. Bonn <jahjae@gmail.com>
2021-09-26 01:48:37 +0700

    jahjae

--- mushaf.erl
+++ mushaf.erl
@@ -1,1 +1,9 @@
 -module(mushaf).
+-export([surat/1, ayat/2]).
+
+surat(X)->
+        X.
+
+ayat(X, Y)->
+        X,
+        Y.
--- qs.erl
+++ qs.erl
@@ -1,5 +1,8 @@
 -module(qs).
--export([page/1])
+-export([page/1, juz/1]).
 
 page(X) ->
+        X.
+
+juz(X)->
         X.
