d65c1c5a8a6ffaee3e0180eab34ab61d333a0660
jahjae <jahjae@gmail.com>
2021-10-10 19:44:38 +0700

    jahjae

--- data.py
+++ data.py
@@ -3,6 +3,7 @@ from init import *
 import csv
 import random
 import threading
+import logging
 
 format = "%(asctime)s: %(message)s"
 logging.basicConfig(format=format, level=logging.INFO,datefmt="%H:%M:%S")
