45692c733839d1484a301f705a977948a4916f05
jahjae <jahjae@gmail.com>
2021-10-10 21:53:48 +0700

    jahjae

--- apps.py
+++ apps.py
@@ -6,7 +6,6 @@ import os
 format = "%(asctime)s: %(message)s"
 logging.basicConfig(format=format, level=logging.INFO,datefmt="%H:%M:%S")
 
-
 def parallel(_func):
     Process(target=_func, args='').start()
 
