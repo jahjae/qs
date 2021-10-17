400985f59fb8ea973ae597c86a521e5df019f7fb
jahjae <jahjae@gmail.com>
2021-10-17 20:25:23 +0700

    jahjae

--- main.ex
+++ main.ex
@@ -1,7 +1,2 @@
 defmodule Qs do
-  use Mushaf
-
-  def surat([]) do
-    surat()
-  end
 end
--- mushaf.ex
+++ mushaf.ex
@@ -1,12 +1,2 @@
 defmodule Mushaf do
-  defmacro surat do
-    quote do
-      {:ok}
-    end
-  end
-  defmacro ayat do
-    quote do
-      {:ok}
-    end
-  end
 end
