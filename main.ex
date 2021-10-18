fa1a15e8a911f30b5cea41a8305137638105af79
jahjae <jahjae@gmail.com>
2021-10-18 08:59:08 +0700

    jahjae

--- main.ex
+++ main.ex
@@ -1,2 +1,16 @@
 defmodule Qs do
+	def start do
+		x = IO.gets "Surat : "
+		y = IO.gets "Ayat  : "
+    	sid = spawn Mushaf, :quran, []
+    	spawn Qs, :mushaf, [sid, x, y]
+  	end
+
+  	def mushaf(sid, x, y) do
+		send sid, {:ayat, self(), x, y}
+    	receive do
+      		{:ayat, arg1, _arg2} ->
+				IO.puts "QS #{arg1}"
+		end
+    end
 end
--- mushaf.ex
+++ mushaf.ex
@@ -1,2 +1,9 @@
 defmodule Mushaf do
+  def quran() do
+    receive do
+      {:ayat, cid, x, y} ->
+        send cid, {:ayat, x, y}
+    end
+
+  end
 end
