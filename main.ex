881f191ae40a2d97abee539bf55883ec07c73362
jahjae <jahjae@gmail.com>
2021-10-26 22:32:52 +0700

    jahjae

--- main.ex
+++ main.ex
@@ -3,29 +3,30 @@ defmodule Qs do
 
 	def surat(args1) do
 		sid = spawn(Mushaf, quran, [])
-		spawn(self(), quran, [sid, arg1])
+		spawn(self(), quran, [sid])
 		send sid, [:surat self(), args1]
 	end
 
 	def ayat(args1, args2) do
 		sid = spawn Mushaf, quran, []
-		spawn self(), quran, [sid, arg1, args2]
+		spawn self(), quran, [sid]
 		send sid, [:ayat self(), args1, args2]
 	end
 
 	def page(args1) do
 		sid = spawn Mushaf, quran, []
-		spawn self(), quran, [sid, self, arg1]
+		spawn self(), quran, [sid]
 		send sid, [:page self(), args1]
 	end
 
-	def quran(sid, [x]) do
+	def quran(sid) do
 		receive do
 			{:ayat, args1, args2} ->
-				IO.puts args1, args2
+				IO.puts :ayat args1, args2
 
 			{:surat, args1}
-		end
+				IO.puts :surat args1
 
+		end
 	end
 end
