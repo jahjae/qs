ee6f404bfc1f828ad266015697a80ad7bec1ffa3
jahjae <jahjae@gmail.com>
2022-04-23 15:17:29 +0700

    jahjae

--- data.py
+++ data.py
@@ -284,5 +284,5 @@ class Q:
         return u
 
     def mushafHuruf(self, u, huruf):
-        u.render('<a class="a" style="line-height: 1.5; text-align: '+ u.props['align']+';font-family: '+ FONTS[u.props['arabicfont']]+ ';font-size: '+ ASIZET[u.props['arabicfontsize']] + '; color: '+ u.props['arabicfontcolor'] +';">' +chr(int(huruf))+ '</a>')
+        u.render('<a class="a" style="line-height: 2; text-align: '+ u.props['align']+';font-family: '+ FONTS[u.props['arabicfont']]+ ';font-size: '+ ASIZET[u.props['arabicfontsize']] + '; color: '+ u.props['arabicfontcolor'] +';">' +chr(int(huruf))+ '</a>')
         return u
