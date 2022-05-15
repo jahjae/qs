93c23d5eac9c3c0a10f7cef8854442bd080ffa96
jahjae <jahjae@gmail.com>
2022-05-15 21:10:19 +0700

    jahjae

--- data.ex
+++ data.ex
@@ -1,10 +1,74 @@
 defmodule Data do
-  def surat do
+  def tafsir() do
 
   end
 
-  def ayat do
+  def code() do
 
+  end
+
+  def surat() do
+
+  end
+
+  def juz() do
+  end
+
+  def halaman() do
+
+  end
+
+  def artiayat() do
+
+  end
+
+  def artikata() do
+
+  end
+
+  def compare() do
+
+  end
+
+  def space() do
+
+  end
+
+  def katabaru() do
+  end
+
+  def artibaru() do
+
+  end
+
+  def tambahbaris() do
+
+  end
+
+  def artiayat()  do
+
+  end
+
+  def artikata() do
+
+  end
+
+  def barisbaru() do
+
+  end
+
+  def periksahuruf() do
+
+  end
+
+  def data() do
+
+  end
+
+  def mushafkata() do
+
+  end
+
+  def mushafhuruf() do
   end
 end
-
